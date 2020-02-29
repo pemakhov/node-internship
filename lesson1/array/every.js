17a6c28aac828998f3ab211e32e7896e21f6097a
pemakhov <serhiy.pemakhov@gmail.com>
2020-02-29 16:01:44 +0200

    feat: add lesson4 Add lesson4 and remove node-modules from the top level

--- lesson1/array/every.js
+++ lesson1/array/every.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, 4, 5, 6];
 
 // all elements are greater than 4
-const greaterFour = arr.every((num) => num > 4);
+const greaterFour = arr.every((num) => num >= 4);
 
 console.log(greaterFour); // output: false
 
